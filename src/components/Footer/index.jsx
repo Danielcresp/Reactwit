839caa8dfc8082633364ed82ee2dbe57480e4a03
Danielcresp <dneli6cres@outlook.com>
2019-04-11 10:11:26 -0600

    Diferencia de Componetes  Stateless y Statefull

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,26 +1,26 @@
-import React ,{Component} from 'react'
+import React from 'react'
 import styles from './footer.css'
 
-class Footer extends Component{
-    render(){
-        return(
-            <div className={styles.root}>
-                <div className={styles.info}>
-                    <p>Reactweet</p>
-                </div>
-                <div className={styles.redes}>
-                    <p className={styles.text}>
-                        <span className='fab fa-facebook-square'></span> Facebook
-                    </p>
-                    <p className={styles.text}>
-                        <span className='fab fa-instagram'></span> Instagram
-                    </p>
-                    <p className={styles.text}>
-                        <span className='fab fa-github'></span> GitHub
-                    </p>
-                </div>
+//Stateless 
+
+function Footer(){
+    return(
+        <div className={styles.root}>
+            <div className={styles.info}>
+                <p>Reactweet</p>
             </div>
-        )
-    }
+            <div className={styles.redes}>
+                <p className={styles.text}>
+                    <span className='fab fa-facebook-square'></span> Facebook
+                </p>
+                <p className={styles.text}>
+                    <span className='fab fa-instagram'></span> Instagram
+                </p>
+                <p className={styles.text}>
+                    <span className='fab fa-github'></span> GitHub
+                </p>
+            </div>
+        </div>
+    )
 }
 export default Footer
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,14 @@
-import React,{Component} from 'react'
+import React from 'react'
 import styles from './header.css'
 
-class Header extends Component{
-    render(){
-        return (
-            <header className={styles.root}>
-                <h1 className={styles.logo}>Reactwit</h1>
-            </header>
-        )
-    }
+//Stateless 
+//este es un compónente sin estado por lo que no necesitamos 
+//crear la clase que herede de Componente pues no tiene estado 
+function Header(){
+    return(
+        <header className={styles.root}>
+          <h1 className={styles.logo}>Reactwit</h1>
+        </header>
+    )
 }
 export default Header
